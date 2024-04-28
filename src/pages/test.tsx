50d50d0bcc9756ef98d3e6313476b703682eefec
Brandon Smith <brandonsmith@Brandons-MacBook-Air.local>
2024-04-27 18:47:19 -0600

    adds test in OG page

---
+++ src/pages/test.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const test = () => {
+  return (
+    <div className='flex justify-center items-center bg-gray-500 h-screen w-full text-2xl text-white font-bold'>
+        Test
+    </div>
+  )
+}
+
+export default test
